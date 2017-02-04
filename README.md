5091844dbb626123d873deb6264039590db33313
gifnksm <makoto.nksm+github@gmail.com>
2017-02-04 21:29:58 +0900

    Update documentaion and crate metadata

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# Noto Sans TTF fonts for embedding into your application binary.
+# Noto Sans TrueType fonts for Rust applications
+
+[![Travis CI Build Status](https://travis-ci.org/gifnksm/ttf-noto-sans.svg?branch=master)](https://travis-ci.org/gifnksm/ttf-noto-sans)
+[![crates.io](https://img.shields.io/crates/v/ttf-noto-sans.svg)](https://crates.io/crates/ttf-noto-sans)
 
 You can use embedded font data from your application.
 
@@ -19,10 +22,11 @@ ttf-noto-sans = { version = "0.1", features = ["bold", "italic", "bold_italic"] 
 ## Example (use with [conrod](https://github.com/PistonDevelopers/conrod))
 
 ```rust
+extern crate conrod;
 use conrod::UiBuilder;
 use conrod::text::FontCollection;
 
-let mut ui = UiBuilder::new([WIDTH as f64, HEIGHT as f64]).build();
+let mut ui = UiBuilder::new([640.0, 480.0]).build();
 let _ = ui.fonts.insert(FontCollection::from_bytes(ttf_noto_sans::REGULAR)
     .into_font()
     .expect("failed to into_font"));
--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,39 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
-//! Noto Sans TTF fonts for embedding into your application binary.
+//! Noto Sans True Type fonts for embedding into your application binary.
+//!
+//! By default, regular style font is embedded.
+//!
+//! ```toml
+//! [package]
+//! ttf-noto-sans = "0.1"
+//! ```
+//!
+//! You can also use other style fonts by specifying custom features.
+//!
+//! ```toml
+//! [package]
+//! ttf-noto-sans = { version = "0.1", features = ["bold", "italic", "bold_italic"] }
+//! ```
+//!
+//! ## Example (use with [conrod](https://github.com/PistonDevelopers/conrod))
+//!
+//! ```rust
+//! # extern crate conrod;
+//! # extern crate ttf_noto_sans;
+//! # fn main() {
+//! use conrod::UiBuilder;
+//! use conrod::text::FontCollection;
+//!
+//! let mut ui = UiBuilder::new([640.0, 480.0]).build();
+//! let _ = ui.fonts.insert(FontCollection::from_bytes(ttf_noto_sans::REGULAR)
+//!     .into_font()
+//!     .expect("failed to into_font"));
+//! # }
+//! ```
+//!
+//! Complete example is [here](examples/conrod_text.rs)
 
 #![warn(bad_style)]
 #![warn(missing_copy_implementations)]
